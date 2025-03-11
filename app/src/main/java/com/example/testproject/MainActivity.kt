95645cd9cdbcd7cc1ae307f079a94769e7ae6c7d
Shalini Mishra <shalini.mishra@iphtechnologies.com>
2025-03-11 12:29:59 +0530

    comment

--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -53,6 +53,7 @@ class MainActivity : AppCompatActivity() {
                 backPressedTime = System.currentTimeMillis().toInt()
             }
         } else {
+            //deprectied method
             super.onBackPressed()
         }
     }
